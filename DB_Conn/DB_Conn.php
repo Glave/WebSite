0936450492bf01132d2801521595b8d18a08f7e0
Glave <glaveye@outlook.com>
2023-06-20 21:42:25 +0800

    数据连接成功

--- DB_Conn/DB_Conn.php
+++ DB_Conn/DB_Conn.php
@@ -1,2 +1,9 @@
-$Link=mssql_connect('GLAVE-WS\GLAVE_DQL_DB',,)
-if()
+<?php
+$server = 'GLAVE-WS\GLAVE_DQL_DB';
+$database = 'Glave_DB';
+$UID = 'Sa';
+$PWD = 'OEdb2017';
+$connection = odbc_connect("Driver={SQL Server Native Client 11.0};Server=$server;Database=$database;", $UID, $PWD);
+if ($connection) {
+    echo "连接成功";
+}
