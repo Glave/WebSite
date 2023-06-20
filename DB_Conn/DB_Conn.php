407cbe1ca3fa0e86cb3dac14d08b3412395c0205
Glave <glaveye@outlook.com>
2023-06-20 21:59:53 +0800

    增加了一个文件，用来关闭数据库

--- DB_Conn/DB_Conn.php
+++ DB_Conn/DB_Conn.php
@@ -1,9 +1,13 @@
 <?php
+//连接字符串
 $server = 'GLAVE-WS\GLAVE_DQL_DB';
 $database = 'Glave_DB';
 $UID = 'Sa';
 $PWD = 'OEdb2017';
+//连接数据库
 $connection = odbc_connect("Driver={SQL Server Native Client 11.0};Server=$server;Database=$database;", $UID, $PWD);
+//检测是否连接成功
 if ($connection) {
     echo "连接成功";
 }
+?>
